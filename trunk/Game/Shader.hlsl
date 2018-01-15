ac58b4fd9a288645be48b8e53c2aa25bcdcb5dad
gzsoft <gzsoft@8f1cf6e8-3d31-4844-a638-a9df89d8468e>
2018-01-15 04:17:03 +0000

    11. Textures

--- trunk/Game/Shader.hlsl
+++ trunk/Game/Shader.hlsl
@@ -3,24 +3,27 @@ cbuffer cbPerObject
 	float4x4 WVP;
 };
 
+Texture2D ObjTexture;
+SamplerState ObjSamplerState;
+
 //struct VS output PS input
 struct POSCOLOR
 {
-	float4 Pos : SV_POSITION;
-	float4 Color : COLOR;
+	float4 Pos :      SV_POSITION;
+	float2 TexCoord : TEXCOORD;
 };
 
-POSCOLOR VS(float4 inPos : POSITION, float4 inColor : COLOR)
+POSCOLOR VS(float4 inPos : POSITION, float2 inTexCoord : TEXCOORD)
 {
 	POSCOLOR output;
 
 	output.Pos = mul(inPos, WVP);
-	output.Color = inColor;
+	output.TexCoord = inTexCoord;
 
 	return output;
 }
 
 float4 PS(POSCOLOR input) : SV_TARGET
 {
-	return input.Color;
+	return ObjTexture.Sample(ObjSamplerState, input.TexCoord);
 }
